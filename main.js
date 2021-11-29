78193294d2741943bdc7c7f99f3a58b7e3159724
Martin Nogueroels <martin.nogueroles@gmail.com>
2021-11-29 02:03:35 -0300

    29/11 0203

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
 
+const spanishBtn = document.querySelector("#spanish_btn")
+
+
 $("button").on("click", (e) =>{
     e.preventDefault();
     $(e.target).toggleClass("button--animate");
-
 }) 
+
+spanishBtn.onclick = (e) => {
+    e.preventDefault();
+    $("#static_txt--en").toggleClass("hide");
+    $("#static_txt--es").toggleClass("active");
+    console.log("hola");
+}
     
